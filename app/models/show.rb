699332bbcb9d27ec98a98806fed7eda373ad0481
Fiona Lapham <fionadlapham@gmail.com>
2018-06-05 09:45:58 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -8,7 +8,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    self.where("rating = #{self.highest_rating}")[0]
+    self.where("rating = #{highest_rating}")[0]
   end
 
   def self.lowest_rating
