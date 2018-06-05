7bea16ae10d83ffb59ffab606b9bdbff65ab9981
Fiona Lapham <fionadlapham@gmail.com>
2018-06-05 09:30:21 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -9,7 +9,7 @@ class Show < ActiveRecord::Base
 
   def self.most_popular_show
     binding.pry
-    Show.where(":rating = ?", "#{Show.highest_rating}".to_i)
+    Show.where("rating: = #{Show.highest_rating}")
   end
 
 end
